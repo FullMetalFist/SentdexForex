e9c2cda07c26c565e33626fdb07d9da0a6a2a141
Michael Vilabrera <m.vilabrera@gmail.com>
2017-04-04 11:32:27 -0400

    percent change & finding patters

--- 01_algo_trading.py
+++ 01_algo_trading.py
@@ -4,10 +4,43 @@ import matplotlib.ticker as mticker
 import matplotlib.dates as mdates
 import numpy as np
 
+date,bid,ask = np.loadtxt('GBPUSD1d.txt', unpack=True,
+                        delimiter=',',
+                        converters={0:mdates.strpdate2num('%Y%m%d%H%M%S')})
+
+def percentChange(startPoint, currentPoint):
+    return ((float(currentPoint)-startPoint)/startPoint)*100.00
+
+def patternFinder():
+    avgLine = ((bid+ask)/2)
+    x = len(avgLine) - 30
+
+    y = 11
+    while y < x:
+        p01 = percentChange(avgLine[y-10], avgLine[y-9])
+        p02 = percentChange(avgLine[y-10], avgLine[y-8])
+        p03 = percentChange(avgLine[y-10], avgLine[y-7])
+        p04 = percentChange(avgLine[y-10], avgLine[y-6])
+        p05 = percentChange(avgLine[y-10], avgLine[y-5])
+        p06 = percentChange(avgLine[y-10], avgLine[y-4])
+        p07 = percentChange(avgLine[y-10], avgLine[y-3])
+        p08 = percentChange(avgLine[y-10], avgLine[y-2])
+        p09 = percentChange(avgLine[y-10], avgLine[y-1])
+        p10 = percentChange(avgLine[y-10], avgLine[y])
+
+        outcomeRange = avgLine[y+20:y+30]
+        currentPoint = avgLine[y]
+
+        print reduce(lambda x, y: x+y, outcomeRange) / len(outcomeRange)
+        print currentPoint
+        print '____'
+
+        print p01, p02, p03, p04, p05, p06, p07, p08, p09, p10
+        y+=1
+        timer.sleep(55)
+
 def graphRawFX():
-    date,bid,ask = np.loadtxt('GBPUSD1d.txt', unpack=True,
-                            delimiter=',',
-                            converters={0:mdates.strpdate2num('%Y%m%d%H%M%S')})
+
 
     fig = plt.figure(figsize=(10,7))
     ax1 = plt.subplot2grid((40,40), (0,0), rowspan=40, colspan=40)
@@ -30,4 +63,5 @@ def graphRawFX():
     plt.grid(True)
     plt.show()
 
-graphRawFX()
+#graphRawFX()
+patternFinder()
